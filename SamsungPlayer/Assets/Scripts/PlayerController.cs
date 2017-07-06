0adc22ec612df4ac113829c976c817eb6ea2d003
cursos <cursos@reina10.aulas.etsiinf.upm.es>
2017-07-06 19:28:55 +0200

    thursday

--- SamsungPlayer/Assets/Scripts/PlayerController.cs
+++ SamsungPlayer/Assets/Scripts/PlayerController.cs
@@ -12,102 +12,134 @@ public class PlayerController : MonoBehaviour {
 	private bool hasLinterna;
 	private bool hasPistola;
 
-    public int hp = 100;
-    public Text pick;
+	public int hp = 100;
+	public Text pick;
 	public Image crosshair;
-    public string basePick = "<b>Recoger";
-    private string buttonText = "\n Pulsa B</b>";
-    private float catchDistance = 3f;
-    private Animator animator;
-    private GameObject rightArm;
+	public string basePick = "<b>Recoger";
+	private string buttonText = "\n Pulsa B</b>";
+	private float catchDistance = 3f;
+	private Animator animator;
+	private GameObject gameController;
+
+
+	//Sonido
+	private AudioSource source;
+	public AudioClip linternaOn;
+	public AudioClip linternaOff;
+	public AudioClip pistolaOn;
+	public AudioClip pistolaOff;
+
+
 
 	// Use this for initialization
 	void Start () {
-        //animator = GetComponentInChildren<Animator>();
-        //rightArm = GameObject.FindGameObjectWithTag("rightArm");
+
+		source = GetComponent<AudioSource> ();
+
+		//animator = GetComponentInChildren<Animator>();
+		//rightArm = GameObject.FindGameObjectWithTag("rightArm");
+		gameController = GameObject.FindGameObjectWithTag("GameController");
 		activeObject = Objetos.Mano;
 		crosshair.enabled = false;
 	}
-	
+
 	// Update is called once per frame
 	void FixedUpdate () {
-		
-        //COGER UN OBJETO
-        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); // al medio de la pantalla
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, catchDistance))
-        {
-            GameObject selectedObject = hit.collider.gameObject;
-            if (selectedObject.tag.Equals("Collectable"))
-            {
-                //SI esta mirando a un objeto coleccionable muestra feedback
-                pick.enabled = true;
-                pick.text = basePick + " " + selectedObject.name + buttonText;
-                //SI pulsa B lo cogemos
-				if (OVRInput.GetDown(OVRInput.Button.Two)/*DEBUG CONTROLL*/ ||Input.GetButtonDown("Fire1"))
-                {
+
+		//COGER UN OBJETO
+		Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); // al medio de la pantalla
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, catchDistance))
+		{
+			GameObject selectedObject = hit.collider.gameObject;
+
+			//OBJETO COLLECTABLE 
+			if (selectedObject.tag.Equals ("Collectable")) {
+				//SI esta mirando a un objeto coleccionable muestra feedback
+				pick.enabled = true;
+				pick.text = basePick + " " + selectedObject.name + buttonText;
+				//SI pulsa B lo cogemos
+				if (OVRInput.GetDown (OVRInput.Button.Two)/*DEBUG CONTROLL*/ || Input.GetButtonDown ("Fire1")) {
 					switch (selectedObject.name) {
-						case "Linterna":
-							hasLinterna = true;
-							activeObject = Objetos.Linterna;
-							crosshair.enabled = false;
-							pistolaObj.SetActive (false);
-							linternaObj.SetActive (true);
-							break;
-						case "Pistola":
-							hasPistola = true;
-							crosshair.enabled = true;
-							activeObject = Objetos.Pistola;
-							linternaObj.SetActive (false);
-							pistolaObj.SetActive (true);
-							break;
+					case "Linterna":
+						hasLinterna = true;
+						activeObject = Objetos.Linterna;
+						crosshair.enabled = false;
+						pistolaObj.SetActive (false);
+						linternaObj.SetActive (true);
+						linternaObj.GetComponent<AudioSource>().PlayOneShot(linternaOn, Random.Range(0.6f, 1f)); //Se reproduce una vez el sonido linternaOff con un volumen entre 0.6 y 1
+						break;
+					case "Pistola":
+						hasPistola = true;
+						crosshair.enabled = true;
+						activeObject = Objetos.Pistola;
+						linternaObj.SetActive (false);
+						pistolaObj.SetActive (true);
+						pistolaObj.GetComponent<AudioSource>().PlayOneShot(pistolaOn, Random.Range(0.6f, 1f)); //Se reproduce una vez el sonido pistolaOff con un volumen entre 0.6 y 1
+						break;
 					}
 					Destroy (selectedObject);
-                }
-            }
-            else
-                pick.enabled = false;
-        }
-        else
-            pick.enabled = false;
+				}
+			} else {
+				pick.enabled = false;
+			}
+
+			//PUERTA
+			if (selectedObject.tag.Equals ("Door")) {
+				if (OVRInput.GetDown (OVRInput.Button.Two)/*DEBUG CONTROLL*/ || Input.GetButtonDown ("Fire1"))
+					selectedObject.SendMessage ("Toogle");
+			}
+		}
+		else
+			pick.enabled = false;
 
 		//TOOGLE LINTERNA
 		if (hasLinterna && (Input.GetKeyDown(KeyCode.E) || OVRInput.GetDown(OVRInput.Button.Three)) /* X */) {
-			
+
 			if (this.activeObject == Objetos.Linterna) {
 				activeObject = Objetos.Mano;
 				linternaObj.SetActive (false);
+				linternaObj.GetComponent<AudioSource>().PlayOneShot(linternaOff, Random.Range(0.6f, 1f)); //Se reproduce una vez el sonido linternaOn con un volumen entre 0.6 y 1
 			} 
 			else {
 				activeObject = Objetos.Linterna;
 				crosshair.enabled = false;
 				pistolaObj.SetActive (false);
 				linternaObj.SetActive (true);
+				linternaObj.GetComponent<AudioSource>().PlayOneShot(linternaOn, Random.Range(0.6f, 1f)); //Se reproduce una vez el sonido linternaOff con un volumen entre 0.6 y 1
+
 			}
 		}
 
 		//TOOGLE PISTOLA
 		if (hasPistola && (Input.GetKeyDown(KeyCode.Q) || OVRInput.GetDown(OVRInput.Button.Four)) /* Y */) {
-			
+
 			if (this.activeObject == Objetos.Pistola) {
 				crosshair.enabled = false;
 				activeObject = Objetos.Mano;
 				pistolaObj.SetActive (false);
+				pistolaObj.GetComponent<AudioSource>().PlayOneShot(pistolaOff, Random.Range(0.6f, 1f)); //Se reproduce una vez el sonido pistolaOn con un volumen entre 0.6 y 1
+
 			} 
 			else {
 				crosshair.enabled = true;
 				activeObject = Objetos.Pistola;
 				linternaObj.SetActive (false);
 				pistolaObj.SetActive (true);
+				pistolaObj.GetComponent<AudioSource>().PlayOneShot(pistolaOn, Random.Range(0.6f, 1f)); //Se reproduce una vez el sonido pistolaOff con un volumen entre 0.6 y 1
+
 			}
 		}
-			
+
 	}
 
-    public void Damage(int value)
-    {
-        hp -= value;
-    }
+	public void Damage(int value)
+	{
+		hp -= value;
+		if (hp == 0) {
+			gameController.SendMessage ("GameOver");
+		}
+	}
 
 
 }
